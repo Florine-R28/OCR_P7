2e009fb8fae052b8de851b44614efee0160f86f8
FLorine-R28 <rodriguesflorine88pgmail.com>
2022-06-15 11:26:00 +0200

    correction api

--- scripts/helpers/api.js
+++ scripts/helpers/api.js
@@ -1,8 +1,8 @@
 //get data file in recipes.js (photographers & medias)
 async function getRecipes() {
-    let url = `scripts/data/recipes.js`; 
+    let url = `data/recipes.json`; 
     let response = await fetch(url);
-    let result = await response.js(); 
+    let result = await response.json(); 
     
     //flip the array
     return ({
